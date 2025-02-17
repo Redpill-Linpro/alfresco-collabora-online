73d4981af79d85cf9d535b2a300195b0bc71ac39
Michael Bui <michael.bui@redpill.linpro.com>
2025-02-17 15:26:18 +0100

    Revert "Revert "Added lock mechanism so collabora is avaiable even if document is locked""
    This reverts commit 5aaa948af7e10737f5058a27990d3dfcf63efef2.

---
+++ collabora-share-extension/src/main/java/org/redpill/alfresco/web/evaluator/NegateEvaluator.java
@@ -0,0 +1,22 @@
+package org.redpill.alfresco.web.evaluator;
+
+import org.alfresco.web.evaluator.BaseEvaluator;
+import org.json.simple.JSONObject;
+
+public class NegateEvaluator extends BaseEvaluator {
+
+    private BaseEvaluator evaluator;
+
+    public void setEvaluator(BaseEvaluator evaluator) {
+        this.evaluator = evaluator;
+    }
+
+    @Override
+    public boolean evaluate(JSONObject jsonObject) {
+        if (evaluator == null) {
+            // You might choose to log a warning here.
+            return false;
+        }//todo remove me if this works
+        return !evaluator.evaluate(jsonObject);
+    }
+}
